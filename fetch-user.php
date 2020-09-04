00563bf2f157487bbf30aa88ab19e45d09d63cf2
praneeth036 <70586253+praneeth036@users.noreply.github.com>
2020-09-04 14:14:35 +0530

    Update fetch-user.php

--- fetch-user.php
+++ fetch-user.php
@@ -7,7 +7,7 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 
 require 'db_connection.php';
 
-$allUsers = mysqli_query($db_conn,"SELECT `aadhar_no`, `admission`, `discharge`, `doctor_id`, `remarks`, `support`, `status`, `hospital_id` FROM `patient-record` WHERE aadhar_no=235");
+$allUsers = mysqli_query($db_conn,"SELECT `aadhar_no`, `admission`, `discharge`, `doctor_id`, `remarks`, `support`, `status`, `hospital_id` FROM `patient-record` WHERE aadhar_no=$_GET["patient_id"]");
 if(mysqli_num_rows($allUsers) > 0){
     $all_users = mysqli_fetch_all($allUsers,MYSQLI_ASSOC);
     echo json_encode(["success"=>1,"users"=>$all_users]);
