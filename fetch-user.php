ad171ce09896890fbb149b4c0d1d0357e89adcbc
praneeth036 <70586253+praneeth036@users.noreply.github.com>
2020-09-04 15:01:24 +0530

    Update fetch-user.php

--- fetch-user.php
+++ fetch-user.php
@@ -6,12 +6,21 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 require 'db_connection.php';
+$data = json_decode(file_get_contents("php://input"));
 
-$allUsers = mysqli_query($db_conn,"SELECT `aadhar_no`, `admission`, `discharge`, `doctor_id`, `remarks`, `support`, `status`, `hospital_id` FROM `patient-record` WHERE aadhar_no=$_GET["patient_id"]");
-if(mysqli_num_rows($allUsers) > 0){
-    $all_users = mysqli_fetch_all($allUsers,MYSQLI_ASSOC);
-    echo json_encode(["success"=>1,"users"=>$all_users]);
+if(isset($data->id) && !empty(trim($data->id))){
+    $id = mysqli_real_escape_string($db_conn, trim($data->id));
+
+    $allUsers = mysqli_query($db_conn,"SELECT `aadhar_no`, `admission`, `discharge`, `doctor_id`, `remarks`, `support`, `status`, `hospital_id` FROM `patient-record` WHERE aadhar_no=$id");
+    if(mysqli_num_rows($allUsers) > 0){
+        $all_users = mysqli_fetch_all($allUsers,MYSQLI_ASSOC);
+        echo json_encode(["success"=>1,"users"=>$all_users]);
+    }
+    else{
+        echo json_encode(["success1"=>0]);
+    }
+
 }
 else{
     echo json_encode(["success"=>0]);
-}
+}  
