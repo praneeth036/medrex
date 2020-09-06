2c85acf19c47e232d621efa25aa30febb86441ae
praneeth036 <70586253+praneeth036@users.noreply.github.com>
2020-09-07 02:05:57 +0530

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,4 +1,4 @@
 <?php
-$db_conn = mysqli_connect("sql12.freemysqlhosting.net","sql12362741","9bA6pA2vkf","sql12362741");
+$db_conn = mysqli_connect("sql12.freemysqlhosting.net","sql12364145","yMEKXy5NHw","sql12364145");
 
 ?>
